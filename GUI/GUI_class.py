e3205d07fac8f90280eb40b2e6de7d377156aa57
Ray Cho <ray.cho94@hotmail.com>
2017-03-05 21:40:54 +1100

    Initial addition of GUI folder and class
    GUI_class.py will act as a class, where separate screens will extend
    from each, with their respective widgets.

---
+++ GUI/GUI_class.py
@@ -0,0 +1,19 @@
+"""
+GUI Code for Potentially T/F
+
+Author: Ray Cho
+Date: 4 March 2017
+
+"""
+from tkinter import *
+
+
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        pass
