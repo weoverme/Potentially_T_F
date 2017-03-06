9d6761810d917771262d97e30f07d036cb1a4603
Ray Cho <ray.cho94@hotmail.com>
2017-03-07 01:24:49 +1100

    Addition of different classes to GUI_class.py and addition of the package, Application.

---
+++ Application/Application.py
@@ -0,0 +1,15 @@
+"""
+Application will be the main driver of all activity.
+Any screen which appears will always be loaded up from this module.
+"""
+
+from GUI import GUI_class as GUI
+import tkinter as tk
+
+class Application():
+
+    def __init__(self):
+        root = tk.Tk()
+        app = GUI.MainGUI(master=root)
+        app.mainloop()
+
--- GUI/GUI_class.py
+++ GUI/GUI_class.py
@@ -1,6 +1,8 @@
 """
 GUI Code for Potentially T/F
 
+Extensions will be made from this module, into other more specific and detailed GUI modules.
+
 Author: Ray Cho
 Date: 4 March 2017
 
@@ -9,7 +11,7 @@ Date: 4 March 2017
 import tkinter as tk
 
 
-class Application(tk.Frame):
+class MainGUI(tk.Frame):
 
     def __init__(self, master=None):
         super().__init__(master)
@@ -17,8 +19,34 @@ class Application(tk.Frame):
         self.master.title("Potentially T/F")
         self.master.minsize(500, 700)
         self.master.maxsize(800, 800)
-        self.create_widgets()
+        self.title_label()
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+    def title_label(self):
+        self.title = tk.Label(self)
+        self.title["text"] = "Potentially T/F"
+        self.title["command"] = None
+        self.title.pack(side="top")
+
+
+class InitialGUI(MainGUI):
+    def __init__(self):
+        MainGUI.__init__(self, master=None)
+
+    def twitter_login_button(self):
+        self.twitter_login = tk.Button(self)
+        self.twitter_login["text"] = "Log into Twitter"
+        self.twitter_login["command"] = None#get_credentials()
+        self.twitter_login.pack(side="bottom")
+
+    def credits_widget(self):
+        self.credits = tk.Label(self)
+        self.credits["text"] = "Credits to Young Rae Cho, Monash University Computer Science Project for Semester 1 2017"
+        self.credits.pack(side="bottom")
+
+
+class DefaultGUI(MainGUI):
+
+    def __init__(self):
+        MainGUI.__init__(self, master=None)
+
+    def
