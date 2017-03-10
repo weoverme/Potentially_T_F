bdea207f8bf9898f9da0c979d00488e4c7514bdd
Ray Cho <ray.cho94@hotmail.com>
2017-03-10 15:42:01 +1100

    Comments and simple documentation added to the file

--- Application/twitter_streaming.py
+++ Application/twitter_streaming.py
@@ -1,17 +1,21 @@
-import json, _json
+import json
 import tweepy
-import sys
-from tweepy import OAuthHandler
 
 
-#My user is  @ray_cho94
+# My user is  @ray_cho94
 access_token = "837846446376271872-PrPyDNixKM7dxCtNrHJ9C73w7XKWzmC"
 access_token_secret = "pNEJZv0tFlPFjrvdg08HEFyPTFv2WmbKmDuFlIS8qQOK9"
 consumer_key = "KOAjR7P0c01Jt8Bs3jAJguXe8"
 consumer_secret = "cG9V3cJPXNogyH7AbvUTZY0aPVhwDkgTUOYk8qsaZ61apYj10L"
 
 
-def function1():
+def main():
+    """
+    The main function of the twitter streaming function. For now
+    the name is only a placeholding name and will be replaced
+    in the near future.
+    :return:
+    """
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
@@ -20,9 +24,17 @@ def function1():
     for friend in tweepy.Cursor(api.friends).items(10):
         process_or_store(friend._json)
         # print(status.text)
-        store_name(friend._json)
+        store_metadata(friend._json)
 
-def store_name(tweet):
+
+def store_metadata(tweet):
+    """
+    Stores the metadata of a tweet._json file into its respective
+    user's name.txt file.
+    :param tweet:
+    :return: None
+
+    """
     name = tweet["name"]
     f = open(name+".txt", "w+")
     for i in tweet:
@@ -31,9 +43,15 @@ def store_name(tweet):
 
 
 def process_or_store(tweet):
+    """
+    Mostly used for testing purposes while getting to know more
+    about the TwitterAPI.
+    :param tweet:
+    :return: None
+    """
     print(json.dumps(tweet))
     print(tweet["name"])
 
 
 
-function1()
+main()
