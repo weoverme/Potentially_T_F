6f0ffcbe1a75110e06581c79639e9bd4625f1710
Ray Cho <ray.cho94@hotmail.com>
2017-03-13 16:49:20 +1100

    Twitter status retrieval working now + implementing File I/O

--- Application/twitter_observer.py
+++ Application/twitter_observer.py
@@ -1,40 +1,60 @@
-import json
 import tweepy
-import traceback
 
-# My user is  @ray_cho94
-access_token = "837846446376271872-PrPyDNixKM7dxCtNrHJ9C73w7XKWzmC"
-access_token_secret = "pNEJZv0tFlPFjrvdg08HEFyPTFv2WmbKmDuFlIS8qQOK9"
-consumer_key = "KOAjR7P0c01Jt8Bs3jAJguXe8"
-consumer_secret = "cG9V3cJPXNogyH7AbvUTZY0aPVhwDkgTUOYk8qsaZ61apYj10L"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+class TwitterObserver:
 
-api = tweepy.API(auth)
+    def __init__(self):
+        # My user is  @ray_cho94
+        self.access_token = "837846446376271872-PrPyDNixKM7dxCtNrHJ9C73w7XKWzmC"
+        self.access_token_secret = "pNEJZv0tFlPFjrvdg08HEFyPTFv2WmbKmDuFlIS8qQOK9"
+        self.consumer_key = "KOAjR7P0c01Jt8Bs3jAJguXe8"
+        self.consumer_secret = "cG9V3cJPXNogyH7AbvUTZY0aPVhwDkgTUOYk8qsaZ61apYj10L"
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
 
-def get_tweets_for(username, n):
-    try:
-        user = api.get_user(username)
-        for tweet in user.timeline().items(n):
-            print(tweet)
-            print(tweet.text)
+        self.api = tweepy.API(self.auth)
 
-        print("done")
-    except Exception:
-        traceback.print_exc()
-
-    except TypeError:
+    def get_tweets_for(self, username, n):
         try:
-            get_tweets_for(username, int(n))
+            for status in tweepy.Cursor(self.api.user_timeline, id=username).items(10):
+                # process_status(status._json)
+                print(status.text)
+                self.write_tweet_to_file(username, status.text)
+        except tweepy.error.TweepError as e:
+            print("Twitter Error: Invalid Twitter Username")
         except TypeError:
-            print("Second argument must be a positive integer value!")
+            try:
+                self.get_tweets_for(username, int(n))
+            except TypeError:
+                print("Second argument must be a positive integer value!")
+
+    def write_tweet_to_file(self, username, tweet):
+        f = open(username + ".txt", "w+")
+        f.write(tweet + "\n")
+        f.close()
+
+    def get_most_recent_tweets(self, username):
+        f = open(username+".txt", "r+")
+        print(len(f))
 
 
-# get_tweets_for("@BarackObama", 10)
+# for status in tweepy.Cursor(api.home_timeline).items(10):
+#     print(status.text)
 
-#for tweet in tweepy.Cursor(api.home_timeline).items(10):
-#    print(json.dumps(tweet._json))
 
-for status in tweepy.Cursor(api.home_timeline).items(5):
-    print(status.text)
+def test_get_tweets_for():
+    test_valid_user = "@ray_cho94"
+    test_invalid_user = "@raydio545"
+    # test for invalid n value
+    testOb = TwitterObserver()
+    testOb.get_tweets_for(test_valid_user, 10.1)
+    print("tested invalid numerical")
+    # test for str version of n value
+    testOb.get_tweets_for(test_valid_user, "10")
+    print("tested str(numerical)")
+    # test for invalid user
+    testOb.get_tweets_for(test_invalid_user, 10)
+    print("tested invalid user")
+
+TwitterObserver().get_tweets_for("@BarackObama", 10)
+TwitterObserver().get_most_recent_tweets("@BarackObama")
