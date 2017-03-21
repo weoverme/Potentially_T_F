9f27a11405a3f2aaaf7188ad8da3f3ee1f41c2b0
Ray Cho <ray.cho94@hotmail.com>
2017-03-21 19:25:04 +1100

    Updated test gui module
    Can now add tabs using custom names with the add button

--- Testing/ttkinterGUI.py
+++ Testing/ttkinterGUI.py
@@ -3,9 +3,17 @@ from tkinter.ttk import *
 
 
 # All functions
-def add_notebook_tab(frame_dic, username, notebook):
-    frame_dic[username] = Frame(notebook)
-    notebook.add(frame_dic[username], text=username)
+def add_notebook_tab():
+    username = get_username_callback()
+#    frame_dic[username] = Frame(notebook)
+    notebook.add(Frame(notebook), text=username)
+
+
+def get_username_callback():
+    val = username_ent.get()
+    username_ent.delete(0, END)
+    username_ent.insert(END, "@")
+    return val
 
 # Root
 root = Tk()
@@ -17,15 +25,28 @@ root.title("Potentially T/F")
 main_frame = Frame(root)
 main_frame.pack(expand='YES', fill='both')
 
+
+# 'Add User' Section
+add_user_frame = Frame(main_frame)
+add_user_frame.pack()
+username_label = Label(add_user_frame)
+username_label["text"] = "Enter a Twitter Handle Here:"
+username_label.pack(side='left', fill='x')
+
+username_ent = Entry(add_user_frame)
+username_ent.insert(0, "@")
+username_ent.pack(side='left', expand='YES', fill='x')
+get_username_button = Button(add_user_frame, text="Add!", command=add_notebook_tab)
+get_username_button.pack(side='left')
+
 #        Notebook
 notebook = Notebook(main_frame)
 notebook.pack(expand='yes', fill='both')
 
 notebook_frames = {}
-add_notebook_tab(notebook_frames, "@BarackObama", notebook)
-add_notebook_tab(notebook_frames, "@realDonaldTrump", notebook)
 notebook.pack()
 
 
-
 root.mainloop()
+
+
