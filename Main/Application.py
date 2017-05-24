280db09cbbec63a3752fad26c0a107232a30332a
Ray Cho <ray.cho94@hotmail.com>
2017-05-24 17:55:11 +1000

    Application successfully connects with twitterWrapper.
    Upon notebook tab creation, TwitterWrapper instantiated for a certain
    user.
    When this happens, it uses a listbox to show the tweets.
    
    TODO: Upon clicking a listing on the listbox, I want to see the text,
    its classification and truthfullness on the description frame below.
    
    Known bugs: Application cannot handle emojis above the range "\FFFF"

--- Main/Application.py
+++ Main/Application.py
@@ -11,11 +11,12 @@ from Main.myClassifier import MyClassifier
 
 class Application(Frame):
 
-    def __init__(self, master=NONE):
+    def __init__(self, master=None):
+
         self.master = master
         Frame.__init__(self, self.master)
-        self.master.minsize(500, 600)
-        self.master.maxsize(500, 700)
+        self.master.minsize(600, 600)
+        self.master.maxsize(700, 700)
         self.master.title("Potentially T/F")
         self.create_widgets()
 
@@ -24,11 +25,11 @@ class Application(Frame):
         self.clf = MyClassifier(load_clf=True)
 
 
-######################################
     def create_widgets(self):
         self.create_main_frame()
         self.create_add_user_frame()
         self.create_notebook_frame()
+        self.create_description_frame()
 
     def create_main_frame(self):
         self.main_frame = Frame(self.master)
@@ -48,14 +49,15 @@ class Application(Frame):
         get_username_button.pack(side='left')
 
     def create_notebook_frame(self):
-        self.notebook = Notebook(self.main_frame, width=480, height=550, padding=5)
+        self.notebook = Notebook(self.main_frame, width=580, height=350, padding=5)
         self.notebook.pack()
 
     def add_notebook_tab(self):
         username = self.get_username_callback()
-
-        tab_frame = Frame(self.notebook)
+        """
         self.tab_holder[username] = []
+
+
         # create a TwitterWrapper for this username
         self.tab_holder[username].append(TwitterWrapper(username, 20))
 
@@ -66,17 +68,23 @@ class Application(Frame):
 
         all_tw_ids = sorted(tw.all_tweets, reverse=True) # all tweet_ids
 
+
         tweet_frames = []
         for tw_id in all_tw_ids:
             tw_text = tw.all_tweets[tw_id]
-            tw_frame = Frame(tab_frame, background='gray', borderwidth='1', padx=2)
-            tw_frame.pack()
+            tw_frame = Frame(tab_frame, borderwidth=1, width=460, height=100)
+            tw_frame.pack(fill='x', side='left', padx=2, pady=1)
 
             # text part of the frame
             tw_frame_text = Label(tw_frame)
-            tw_frame_text['text'] = tw_text
-            tw_frame_text.pack(side='left', fill='x')
+#            tw_frame_text['text'] = tw_text
 
+            try:
+                tw_frame_text['text'] = tw_text
+                tw_frame_text.pack(side='left', fill='x')
+                input("Press enter")
+            except TclError:
+                print(tw_text)
 
 
 
@@ -88,10 +96,44 @@ class Application(Frame):
 
 
         self.tab_holder[username].append(tweet_frames)
+        """
+
+        tab_frame = Frame(self.notebook)
+
+        self.tab_holder[username] = TwitterWrapper(username, 20)
         self.notebook.add(tab_frame, text=username)
 
+        tweet_listbox = Listbox(tab_frame, width=100, height=20, selectmode=SINGLE) # width=X-chars, height=Y-lines
+        #tweet_listbox.pack()
+        self.add_tweets_to_listbox(username, tweet_listbox)
+        tweet_listbox.pack()
 
-###############
+    def add_tweets_to_listbox(self, username, listbox):
+        user_tweet_ids = sorted(self.tab_holder[username].all_tweets.keys())
+
+        for tw_id in user_tweet_ids:
+            tw_text = self.tab_holder[username].all_tweets[tw_id]
+            # append to end of listbox
+            try:
+                listbox.insert(END, tw_text)
+            except TclError:
+                print(tw_text)
+                pass
+
+    def create_description_frame(self):
+        self.desc_frame = LabelFrame(self.main_frame, text="Tweet Description")
+        self.desc_frame.configure(borderwidth=1)
+        self.desc_frame.pack()
+
+        self.desc_text = Label(self.desc_frame)
+        self.desc_text.pack()
+        self.update_desc_contents("text")
+
+    def update_desc_contents(self, new_text):
+        self.desc_text['text'] = new_text
+        self.desc_text.pack()
+
+    ###############
     def get_username_callback(self):
         val = self.username_ent.get()
         self.username_ent.delete(0, END)
